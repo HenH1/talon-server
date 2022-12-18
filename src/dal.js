9b11a6ec656f3e66c947ed970cc610222ce4e2c0
“Hen <hershkohen@gmail.com>
2022-12-18 21:02:10 +0200

    improving the code

--- src/dal.js
+++ src/dal.js
@@ -6,14 +6,13 @@ dotenv.config();
 
 // Connection URL
 const url = process.env.MONGO_CLIENT_URL;
-export const client = new MongoClient(url);
+const client = new MongoClient(url);
 const dbName = 'talonDB';
 const collectionName = 'systemEvents';
 
 async function initDB() {
     await client.connect();
     const db = client.db(dbName);
-
     // Check if we already have collection with data
     const collectionExists = (await db.listCollections().toArray()).some(collection => collection.name === collectionName);
     if (!collectionExists) {
@@ -23,30 +22,24 @@ async function initDB() {
 }
 // Returning resuslt as: []
 async function getAllEventTypes() {
-    const coll = client.db('talonDB').collection('systemEvents');
-    const systemEvents = await coll.distinct("eventType");
+    const collection = client.db('talonDB').collection('systemEvents');
+    const systemEvents = await collection.distinct("eventType");
 
     return systemEvents;
 }
 // Returning resuslt as: {count: number ,data:[]}
 async function getFilteredEvents(eventsName, amount, skip) {
-    const coll = client.db('talonDB').collection('systemEvents');
-    let queryVar = [];
+    const collection = client.db('talonDB').collection('systemEvents');
+    const queryVar = eventsName.map(eventTypeName => ({ eventType: eventTypeName }));
+    const filter = queryVar.length > 0 ? { $or: queryVar } : {};
     const options = {
         sort: { time: -1 },
         limit: amount,
         skip: skip
     };
-
-    for (let i = 0; i < eventsName.length; i++) {
-        queryVar.push({ eventType: eventsName[i] });
-    }
-
-    const filter = queryVar.length > 0 ? { $or: queryVar } : {};
-
-    const cursor = coll.find(filter, options);
+    
+    const cursor = collection.find(filter, options);
     const eventsArray = await cursor.toArray();
-
     const count = await client.db('talonDB').collection('systemEvents').countDocuments(filter);
 
     return { count: count, data: eventsArray };
--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -6,20 +6,28 @@ router.get('/', (req, res) => {
     res.send('Ok');
 });
 
-router.get('/getEventTypes', (req, res) => {
-    getAllEventTypes().then((eventTypes) =>
-        res.send(eventTypes)
-    ).catch((err) => res.send(err.message));
+router.get('/getEventTypes', async (req, res) => {
+    try {
+        const eventTypes = await getAllEventTypes();
+        res.send(eventTypes);
+    }
+    catch (err) {
+        res.send(err.message);
+    };
 });
 
-router.get('/events/page/:pageNum/amount/:amountPerPage/', (req, res) => {
+router.get('/events/page/:pageNum/amount/:amountPerPage/', async (req, res) => {
     const eventsNameArray = req.query.eventTypes?.split(",") || [];
     const amount = parseInt(req.params.amountPerPage);
     const skip = parseInt(req.params.pageNum) * amount;
 
-    getFilteredEvents(eventsNameArray, amount, skip).then((eventTypes) =>
-        res.send(eventTypes)
-    ).catch((err) => res.send(err.message));
+    try {
+        const events = await getFilteredEvents(eventsNameArray, amount, skip);
+        res.send(events);
+    }
+    catch (err) {
+        res.send(err.message);
+    };
 });
 
 export { router };
